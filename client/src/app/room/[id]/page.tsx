f30c1fafda831a38d2b87ef354c31fa0c7c47465
eduardDorzhinov <eddorzh@mail.ru>
2025-10-18 11:37:25 +0200

    change props type

--- client/src/app/room/[id]/page.tsx
+++ client/src/app/room/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { RoomPage } from "@/ui/pages/room/RoomPage";
 
-interface Props { params: { id: string }; }
+type Props = { params: Promise<{ id: string }> };
 
 export default async function Page({ params }: Props) {
   const { id } = await params;
